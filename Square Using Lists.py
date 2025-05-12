913852e05a8afada85c487d387623e78832dd77e
Akshaj6 <akshaj.samaria@gmail.com>
2025-05-12 15:30:53 +0530

    60

--- Square Using Lists.py
+++ Square Using Lists.py
@@ -1,18 +1,19 @@
-def find_and_filter_squares(start, end):
-    all_squares = []
+def find_squares(start, end):
+    squares = []
     even_squares = []
     odd_squares = []
     for num in range(start, end + 1):
-        square = num**2
-        all_squares.append(square)
+        square = num ** 2
+        squares.append(square)
         if square % 2 == 0:
             even_squares.append(square)
         else:
             odd_squares.append(square)
-        print("All squares :", all_squares)
-        print("Even squares :", even_squares)
-        print("Odd squares :", odd_squares)
+    return squares, even_squares, odd_squares
 print("This program calculates the square values and separates odd/even values.")
 start = int(input("Enter the starting number :"))
 end = int(input("Enter the ending number :"))
-find_and_filter_squares(start, end)
+squares, even_squares, odd_squares = find_squares(start, end)
+print("Squares :", squares)
+print("Even squares :", even_squares)
+print("Odd squares :", odd_squares)
